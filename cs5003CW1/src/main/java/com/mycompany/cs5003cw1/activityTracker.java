39d8314ddc0851959449b9689bd1f4ffa9059edd
abdullahuni333 <abdullahalnahasbusiness@gmail.com>
2025-11-13 21:14:20 +0000

    forgot to put the code inside

--- cs5003CW1/src/main/java/com/mycompany/cs5003cw1/activityTracker.java
+++ cs5003CW1/src/main/java/com/mycompany/cs5003cw1/activityTracker.java
@@ -6,8 +6,44 @@ package com.mycompany.cs5003cw1;
 
 /**
  *
- * @author agant
+ * @author aba2059
  */
 public class activityTracker {
-    
+
+    int activityID;
+    String activityName;
+    int activityQauntity;
+    String activityDate;
+
+    void set_ID(int ID) {
+        activityID = ID;
+    }
+
+    int get_ID() {
+        return activityID;
+    }
+
+    void set_name(String name) {
+        activityName = name;
+    }
+
+    String get_name() {
+        return activityName;
+    }
+
+    void set_Qauntity(int Qauntity) {
+        activityQauntity = Qauntity;
+    }
+
+    int get_Qauntity() {
+        return activityQauntity;
+    }
+
+    void set_date(String date) {
+        activityDate = date;
+    }
+
+    String get_date() {
+        return activityDate;
+    }
 }
