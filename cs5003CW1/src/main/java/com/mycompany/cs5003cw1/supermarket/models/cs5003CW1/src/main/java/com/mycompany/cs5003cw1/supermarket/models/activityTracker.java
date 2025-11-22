de5df379e4f67ce60f8090da57b5fc0d49e6e531
abdullahuni333 <abdullahalnahasbusiness@gmail.com>
2025-11-22 23:34:21 +0000

    should fix things

---
+++ cs5003CW1/src/main/java/com/mycompany/cs5003cw1/supermarket/models/cs5003CW1/src/main/java/com/mycompany/cs5003cw1/supermarket/models/activityTracker.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cs5003cw1.supermarket.models;
+
+/**
+ *
+ * @author aba2059
+ */
+public class activityTracker extends Product {
+
+
+    
+    public activityTracker(int productID, String productName, String entryDate, int quantity){
+        // inherits all the attributes of the product class to use it 
+        super(productID,productName,entryDate,quantity);
+
+    
+    }
+
+
+}
